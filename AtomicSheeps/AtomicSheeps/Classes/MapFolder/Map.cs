a9fb700abd4c6489eefa03d14167d9809b042741
Kedreals <ebert.mueden1@freenet.de>
2015-11-21 14:33:03 +0100

    Half the Map.cs

---
+++ AtomicSheeps/AtomicSheeps/Classes/MapFolder/Map.cs
@@ -0,0 +1,37 @@
+﻿using AtomicSheeps.Core;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtomicSheeps.Classes.MapFolder
+{
+    class Map
+    {
+        Tile[,] Tiles;
+        float TileSize = 32;
+
+        static Color white = Color.FromArgb(255, 255, 255);
+        static Color black = Color.FromArgb(0, 0, 0);
+
+        public Map(Bitmap bMap)
+        {
+            Tiles = new Tile[bMap.Width, bMap.Height];
+            for(int i = 0; i < Tiles.GetLength(0); ++i)
+                for(int j = 0; j<Tiles.GetLength(1); ++j)
+                {
+                    if (bMap.GetPixel(i, j).Equals(white))
+                        Tiles[i, j] = new Tile(false, new Vec2f(i, j) * TileSize);
+                    else
+                    {
+                        if (bMap.GetPixel(i, j).Equals(black))
+                        {
+                            
+                        }
+                    }
+                }
+        }
+    }
+}
---
+++ AtomicSheeps/AtomicSheeps/Classes/MapFolder/Tile.cs
@@ -0,0 +1,27 @@
+﻿using AtomicSheeps.Core;
+using SFML.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtomicSheeps.Classes.MapFolder
+{
+    class Tile
+    {
+        Sprite S;
+        public bool IsPath { get; private set; }
+
+        public Tile(bool isPath, Vec2f WorldPosition, string texturePath = "Assets/Textures/grass.png")
+        {
+            S = new Sprite(new Texture(texturePath));
+            IsPath = isPath;
+        }
+
+        public void Draw(RenderWindow win)
+        {
+            win.Draw(S);
+        }
+    }
+}
