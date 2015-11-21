cedbbe1ff3aa9a1073cc48bc0eb5d576cc960555
Joshua Endert <joshua95@gmx.net>
2015-11-21 18:15:49 +0100

    stufff

---
+++ AtomicSheeps/AtomicSheeps/Classes/GameObjects/Tower/AbstractTower.cs
@@ -0,0 +1,39 @@
+﻿using AtomicSheeps.Core;
+using SFML.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtomicSheeps.Classes.GameObjects.Tower
+{
+    abstract class AbstractTower
+    {
+        protected Sprite sprite;
+        public float damage { get; protected set; }
+        public float range { get; protected set; }
+        public float costs { get; protected set; }
+
+        public AbstractTower()
+        {
+            TowerHandler.Towers.Add(this);
+        }
+
+        public Vec2f Position { get { return sprite.Position; } }
+
+        protected void Shooting()
+        {
+
+        }
+
+        public void Update(GameTime gTime)
+        {
+
+        }
+        public void Draw(RenderWindow win)
+        {
+            win.Draw(sprite);
+        }
+    }
+}
