606eef117292a17c571ebfe5fe149a3d89ee831f
Kedreals <ebert.mueden1@freenet.de>
2015-11-21 16:36:32 +0100

    Nüx

--- AtomicSheeps/AtomicSheeps/Classes/GameStates/InGame.cs
+++ AtomicSheeps/AtomicSheeps/Classes/GameStates/InGame.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using AtomicSheeps.Core;
+﻿using AtomicSheeps.Core;
 using SFML.Graphics;
 using AtomicSheeps.Classes.MapFolder;
 using SFML.Audio;
